e06122ab975d4919131d5424fd4f126e03176e2b
SatyamMattoo <satyammattoo2003@gmail.com>
2023-10-02 18:59:18 +0530

    initial commit

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://hpuejournal.netlify.app/api/v1/users",
+    baseUrl: "https://hpuejournal.onrender.com/api/v1/users",
   }),
   endpoints: (builder) => ({
     register: builder.mutation({
