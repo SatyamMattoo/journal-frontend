a9618970feccdf0da4fe50fd4521dd1f60de4275
SatyamMattoo <satyammattoo2003@gmail.com>
2024-04-07 23:08:24 +0530

    fixing typo

--- src/store/api/articleApi.js
+++ src/store/api/articleApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1",
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
   }),
   endpoints: (builder) => ({
     //Published Articles
